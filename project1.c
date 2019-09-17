f65356666f3aac9a00837e6fdd7772797c0ef447
mcech99 <34220949+mcech99@users.noreply.github.com>
2019-09-17 10:26:51 -0400

    idk what i changed

--- project1.c
+++ project1.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "time_functions.h"
-#include "setpath_dets.h"
+//#include "time_functions.h"
+#include "setpath_defs.h"
 
-void read_chars(FILE *fp);
-void read_lines(FILE *fp);
+void read_chars(FILE *fp,FILE *outfp);
+void read_lines(FILE *fp,FILE *outfp);
 
-int main(int argc, char **argv[]){
+int main(int argc, char *argv[]){
 
 	FILE *fp;
 	FILE *outfp;
+	char buff[80];
 
 	if (argc != 3){
 		printf("Usage: %s filename mode\n", argv[0]);
@@ -22,20 +23,23 @@ int main(int argc, char **argv[]){
 		printf("Can't open %s\n", argv[1]);
 		return 1;
 	}
+	setvbuf(fp,buff,_IONBF,80);
 	outfp = fopen("outfile.txt","w"); // CHANGE 
+	setvbuf(outfp,buff,_IONBF,80);
 	
 	//request mode for program to use
 	int mode = atoi(argv[2]);
 
 
 	if(mode==0){
-		start_timing();
-		read_chars(inpath+infile);
-		stop_timing();
-	} else if(mode==1){
-		start_timing();
-		read_lines(inpath + infile);
-		stop_timing();
+		//start_timing();
+		read_chars(fp,outfp);
+		//stop_timing();
+	} 
+	else if(mode==1){
+		//start_timing();
+		read_lines(fp,outfp);
+		//stop_timing();
 	}
 	else {
 		printf("Incorrect mode input, mode must be either '1' for line-by-line modeor '0' for character-by-character mode");
@@ -47,10 +51,12 @@ int main(int argc, char **argv[]){
 	fclose(outfp);
 
 	//compute and display times
-	double x = get_wall_clock_dif(); //wall clock difference
-	int y = get_nanodiff(); //CPU clock difference
-
-	pause311();
+	//double x = get_wall_clock_dif(); //wall clock difference
+	//int y = get_nanodiff(); //CPU clock difference
+	double x = 0.00;
+	int y = 0;
+	printf("Wall clock time: %f\nCPU time: %d\n",x,y);
+	//pause311();
 
 	return 0;
 }
